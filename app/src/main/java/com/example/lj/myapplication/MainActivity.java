e7087a3da49b49a6487928c3598d4b23f0eb12a4
LJ-PC\lj <leejunghwan90@gmail.com>
2017-12-29 20:21:33 +0900

    GPS권한설정완료 GPS작동 완료

--- app/src/main/java/com/example/lj/myapplication/MainActivity.java
+++ app/src/main/java/com/example/lj/myapplication/MainActivity.java
@@ -1,10 +1,17 @@
 package com.example.lj.myapplication;
 
+import android.Manifest;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
+import android.widget.Toast;
+
+import com.gun0912.tedpermission.PermissionListener;
+import com.gun0912.tedpermission.TedPermission;
+
+import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -16,6 +23,7 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         Button button = (Button)findViewById(R.id.btnGoogleMap);
+        Button gpsBtn = (Button)findViewById(R.id.btnGPS);
 
         button.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -24,5 +32,33 @@ public class MainActivity extends AppCompatActivity {
                 startActivity(intent);
             }
         });
+        gpsBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(MainActivity.this,GpsActivity.class);
+                startActivity(intent);
+            }
+        });
+        PermissionListener permissionlistener = new PermissionListener() {
+            @Override
+            public void onPermissionGranted() {
+                Toast.makeText(MainActivity.this, "권한 허가", Toast.LENGTH_SHORT).show();
+            }
+
+            @Override
+            public void onPermissionDenied(ArrayList<String> deniedPermissions) {
+                Toast.makeText(MainActivity.this, "권한 거부\n" + deniedPermissions.toString(), Toast.LENGTH_SHORT).show();
+            }
+
+
+        };
+        TedPermission.with(this)
+                .setPermissionListener(permissionlistener)
+                .setRationaleMessage("구글 로그인을 하기 위해서는 주소록 접근 권한이 필요해요")
+                .setDeniedMessage("왜 거부하셨어요...\n하지만 [설정] > [권한] 에서 권한을 허용할 수 있어요.")
+                .setPermissions(Manifest.permission.ACCESS_FINE_LOCATION)
+                .check();
+
+
     }
 }
