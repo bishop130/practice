88d7d8e201edc898f8707339dafadc765434fddd
JUNGHWAN\junghwan <leejunghwan90@gmail.com>
2018-03-24 21:23:39 +0900

    데이터 설정준비

---
+++ app/src/main/java/com/example/lj/myapplication/ResultActivity.java
@@ -0,0 +1,79 @@
+package com.example.lj.myapplication;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.android.volley.AuthFailureError;
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
+
+import java.io.StringReader;
+import java.util.HashMap;
+import java.util.Map;
+
+public class ResultActivity extends AppCompatActivity {
+    String result;
+    private StringRequest request;
+    private static final String URL = "http://bishop130.cafe24.com/test.php";
+    private RequestQueue requestQueue;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_result);
+
+        requestQueue = Volley.newRequestQueue(this);
+
+
+        Intent intent = getIntent();
+        String date = intent.getStringExtra("date");
+        String time = intent.getStringExtra("time");
+        String location = intent.getStringExtra("Location");
+        result = "date:"+date+"time:"+time+"location"+location;
+
+        Button button = (Button)findViewById(R.id.Btn_Register);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                request = new StringRequest(Request.Method.POST, URL, new Response.Listener<String>() {
+                    @Override
+                    public void onResponse(String response) {
+
+                        Toast.makeText(getApplicationContext(),"전송완료",Toast.LENGTH_LONG).show();
+
+                    }
+                }, new Response.ErrorListener() {
+                    @Override
+                    public void onErrorResponse(VolleyError error) {
+
+                    }
+                }){
+                    @Override
+                    protected Map<String, String> getParams() throws AuthFailureError {
+                        HashMap<String,String> hashMap = new HashMap<String, String>();
+                        hashMap.put("email",result);
+                        hashMap.put("password","bishop130");
+
+                        return hashMap;
+                    }
+                };
+                requestQueue.add(request);
+
+            }
+        });
+
+
+
+        TextView textView = (TextView)findViewById(R.id.textView);
+        textView.setText(result);
+    }
+}
